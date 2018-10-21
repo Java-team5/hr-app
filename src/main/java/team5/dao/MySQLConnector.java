5263231d64be5c4281995eed72acb71be8e62919
Vladuken <vladpetrushkevich1999@gmail.com>
2018-10-21 16:44:55 +0300

    Added singleton connector + tests

--- src/main/java/team5/dao/MySQLConnector.java
+++ src/main/java/team5/dao/MySQLConnector.java
@@ -6,38 +6,28 @@ import java.sql.SQLException;
 
 public class MySQLConnector {
     //TODO change sqlconnection properties to properties file
-    private static final String URL="jdbc:mysql://localhost:3306/hr_app_db";
+    private static final String URL="jdbc:mysql://localhost:3306/hr_app_db?autoReconnect=true&useSSL=false";
     private static final String USERNAME="root";
     private static final String PASSWORD="";
-    private static boolean singletonFlag = false;
-    private static Connection connection = null;
 
-    public static Connection getConnection() {
-        return connection != null ? connection : null;
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException,ClassNotFoundException{
+        if (connection == null)
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection =  DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        }
+        return connection;
     }
 
-    private MySQLConnector() throws SQLException, ClassNotFoundException{
-        Class.forName("com.mysql.jdbc.Driver");
-        MySQLConnector.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    public static void closeConnection(Connection con) throws SQLException{
+        if(connection.equals(con))
+            if(connection!=null)
+            {
+                connection.close();
+                connection=null;
+            }
     }
 
-    private MySQLConnector getMySQLConnector() throws SQLException, ClassNotFoundException{
-        try {
-            return !singletonFlag ? new MySQLConnector() : null;
-        }
-        catch (SQLException e){
-            throw e;
-        }
-        catch (ClassNotFoundException e){
-            throw new ClassNotFoundException("JDBC Driver was not found");
-        }
-    }
-
-    public static void close(MySQLConnector connector) throws SQLException{
-        if(MySQLConnector.singletonFlag){
-            connector = null;
-            MySQLConnector.connection.close();
-            MySQLConnector.singletonFlag = false;
-        }
-    }
 }
