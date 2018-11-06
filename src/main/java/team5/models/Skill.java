2304836488a70b3dc51d3f6ec895d23c39145333
Gurinov <evgeniy-gurinov@mail.ru>
2018-11-07 00:56:27 +0300

    Interview to REST. Add tests for models and controllers(coverage 100%). Fix all checkStyle bugs for 'Skill' and 'Interview' entities. Delete skillFilter. Make name 'team5' is valid for checkStyle. Make '*' in imports is valid.

--- src/main/java/team5/models/Skill.java
+++ src/main/java/team5/models/Skill.java
@@ -3,42 +3,23 @@ package team5.models;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-/**
- * Entity 'Skill'.
- */
 public class Skill {
 
-    /**
-     * PK.
-     */
     @NotNull
     @Size(min = 1, max = 50)
     private String skill;
 
-    /**
-     * @return skill.
-     */
     public String getSkill() {
         return skill;
     }
 
-    /**
-     * @param skill new skill value.
-     */
     public void setSkill(final String skill) {
         this.skill = skill;
     }
 
-    /**
-     * Constructor.
-     */
     public Skill() {
     }
 
-    /**
-     * Constructor.
-     * @param skill Skill value.
-     */
     public Skill(final String skill) {
         this.skill = skill;
     }
